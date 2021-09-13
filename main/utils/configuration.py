b7d93a1252deb31ac8122ac6ea3a7d94dcfcf4d0
SumitJainUTD <sumi12june@gmail.com>
2021-09-12 22:23:03 -0500

    remove user from test data added user to config file add post api in config

--- main/utils/configuration.py
+++ main/utils/configuration.py
@@ -1,18 +1,27 @@
 import configparser
 
+import requests
+
 
 class Configuration(object):
     def __init__(self, env):
+        self.users = None
         config = configparser.ConfigParser(allow_no_value=True)
         if env == 'qa':
             config.read('resources/project-qa.cfg')
         elif env == 'staging':
             config.read('resources/project-lt.cfg')
-        elif env == 'pr0d':
+        elif env == 'prod':
             config.read('resources/project-prd.cfg')
         else:
             config.read('project-qa.cfg')
 
-        self.base_uri = config.get('ConfigurationSection', 'base_uri')
-        self.auth_uri = config.get('ConfigurationSection', 'auth_uri')
-        self.db_host = config.get('ConfigurationSection', 'audience_uri')
+        self.base_uri = config.get('GlobalSection', 'base_uri')
+        self.auth_uri = config.get('GlobalSection', 'auth_uri')
+        self.post_uri = config.get('GlobalSection', 'post_uri')
+        self.create_post_uri = config.get('GlobalSection', 'create_post_uri')
+        self.update_post_uri = config.get('GlobalSection', 'update_post_uri')
+        self.db_host = config.get('GlobalSection', 'audience_uri')
+        self.users['first_admin_user'] = {'username': config.get('FirstAdminSection', 'username'),
+                                          'password': config.get('FirstAdminSection', 'password')}
+        self.SESSION = requests.Session()
---
+++ test/conftest.py
@@ -0,0 +1,48 @@
+import pytest
+
+from main.utils.configuration import Configuration
+from main.auth import Auth
+
+env = None
+logged_in_user = {}
+
+
+def pytest_addoption(parser):
+    parser.addoption("--env")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def before_suite(request):
+    print('Beginning of the suite')
+    global env
+    env = request.config.getoption("--env")
+    properties = Configuration(env)
+
+    def after_suite():
+        print('End of the suite')
+
+    request.addfinalizer(after_suite)
+
+
+def get_env():
+    return env
+
+
+def get_token_for_user(user, refresh=False):
+    tokens = logged_in_user.get(user)
+    auth = Auth()
+    if refresh:
+        tokens = auth.login(refresh_token=tokens.get('refresh'))
+        logged_in_user[user] = tokens
+    else:
+        username = user.get('username')
+        password = user.get('password')
+        # login the user
+        if tokens is None:
+            tokens = auth.login(username=username, password=password)
+            logged_in_user[user] = tokens
+        else:
+            if not auth.verify_token(tokens.get('access')):
+                return get_token_for_user(user=user, refresh=True)
+
+    return tokens
